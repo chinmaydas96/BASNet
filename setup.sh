fc26d86422757a43bc260849f0a022d41564ebc2
chinmaydas96 <chinmaydasbat@gmail.com>
2020-06-06 00:34:14 +0530

    new scripts

---
+++ setup.sh
@@ -0,0 +1,13 @@
+wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+bash Anaconda3-2020.02-Linux-x86_64.sh 
+
+
+pip install gdown opencv-python
+
+
+
+### Copy google drive data to server
+### 	1. copy shareable link
+###	    2. Click on download and copy that link
+
+
